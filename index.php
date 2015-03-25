9ac6e6874c7cfa0e700267f6ca622977822045d6
patpiech88 <patrick@piechatzekpk.de>
2015-03-25 10:56:49 +0100

    SQL Anfang Test
    Datenbank Input ??

---
+++ index.php
@@ -0,0 +1,48 @@
+﻿<?php
+
+ ?>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="css/common.css"/>
+		<title>Willkommen bei den Sportfreunden der RFH Köln!</title>
+	</head>
+<body>
+	<div>
+		<h1>Willkommen bei den Sportfreunden der RFH Köln!</h1>
+	</div>
+	<div id="menu">
+		<table>
+			<th>
+				User Menü
+			</th>
+			<tr>
+				<td>
+					<a href="..."> Login</a>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<a href="..."> Profil</a>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<a href="..."> Statistik</a>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<a href="..."> Lauf eintragen</a>
+				</td>
+			</tr>
+		</table>
+	</div>
+	<div id="hauptteil">
+		
+		<label for="Neuer_Lauf"> Neuen Lauf eintragen </label>
+		<input type="text" name="Neuer_Lauf"/>
+		<input type="submit" name="save" value="Eintragen"/>
+	</div>
+</body>
+</html>
