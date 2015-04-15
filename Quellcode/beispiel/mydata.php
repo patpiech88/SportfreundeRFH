f15e02876edb2bdb9d65e569817099a53ed888d2
patpiech88 <patrick@piechatzekpk.de>
2015-04-15 08:27:35 +0200

    Datenabgleich vom FTP Server

--- Quellcode/beispiel/mydata.php
+++ Quellcode/beispiel/mydata.php
@@ -1,3 +1,19 @@
 <?php
 		echo "<p> Hier die Userdaten/Profil!</p>";
 ?>
+
+<html>
+<head>
+<meta charset="UTF-8"> 
+<title>Sportfreunde</title>
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+
+</head>
+<body >
+<div id="wrapper">
+        <div id="header">
+		Test	
+		</div>
+</div>
+</body>
+</html>
